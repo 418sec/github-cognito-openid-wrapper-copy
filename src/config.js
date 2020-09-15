33c6a1118fb19b75b0c7c31a9e6cf94904936f4d
Adam Nygate <adam@418sec.com>
2020-09-15 09:41:46 +0100

    Correcting env names

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: process.env.GH_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GH_CLIENT_SECRET,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   COGNITO_REDIRECT_URI: process.env.COGNITO_REDIRECT_URI,
   GITHUB_API_URL: process.env.GITHUB_API_URL, // unused
   GITHUB_LOGIN_URL: process.env.GITHUB_LOGIN_URL, // unused
